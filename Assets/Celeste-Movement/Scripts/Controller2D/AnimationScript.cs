3368d457b9169b8a5b35bed4ff82bd764079bb82
snaptothegrid <snaptothegrid@gmail.com>
2022-08-22 14:01:32 +0900

    Swappable animated heroes :)

--- Assets/Celeste-Movement/Scripts/Controller2D/AnimationScript.cs
+++ Assets/Celeste-Movement/Scripts/Controller2D/AnimationScript.cs
@@ -1,33 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 namespace Carles.Engine2D {
 
   public class AnimationScript : MonoBehaviour {
 
-    private Animator anim;
+    public SpriteLibraryAsset[] spriteLibs;
+    public int splIndex;
+
     private Movement move;
     private Collision coll;
-    [HideInInspector]
-    public SpriteRenderer sr;
+    private Animator anim;
+    [HideInInspector] public SpriteRenderer sr;
 
     void Start() {
-      anim = GetComponentInParent<Animator>();
       coll = GetComponentInParent<Collision>();
       move = GetComponentInParent<Movement>();
+      anim = GetComponent<Animator>();
       sr = GetComponent<SpriteRenderer>();
+
+      SetSpriteLibraryRandom();
     }
 
     void Update() {
+      anim.SetBool("canMove", move.canMove);
       anim.SetBool("onGround", coll.onGround);
       anim.SetBool("onWall", coll.onWall);
       anim.SetBool("onRightWall", coll.onRightWall);
       anim.SetBool("wallGrab", move.wallGrab);
       anim.SetBool("wallSlide", move.wallSlide);
-      anim.SetBool("canMove", move.canMove);
       anim.SetBool("isDashing", move.isDashing);
-
     }
 
     public void SetHorizontalMovement(float x, float y, float yVel) {
@@ -49,6 +53,17 @@ namespace Carles.Engine2D {
 
       bool state = (side == 1) ? false : true;
       sr.flipX = state;
+    }
+
+    public void SetSpriteLibraryRandom() {
+
+      SetSpriteLibrary(Random.Range(0, spriteLibs.Length));
+    }
+
+    public void SetSpriteLibrary(int index) {
+      splIndex = index;
+      SpriteLibrary spl = GetComponent<SpriteLibrary>();
+      spl.spriteLibraryAsset = spriteLibs[splIndex];
     }
   }
 
---
+++ Assets/Celeste-Movement/Scripts/RandomChar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Carles.Engine2D {
+
+  public class RandomChar : MonoBehaviour {
+
+    void Start() {
+
+    }
+
+  }
+}
