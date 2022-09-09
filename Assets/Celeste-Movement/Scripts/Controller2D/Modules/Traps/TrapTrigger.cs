7fcf281a1dc5332a0894b006dd97e25a3b622cbc
snaptothegrid <snaptothegrid@gmail.com>
2022-09-10 02:13:30 +0900

    preparations for TrapTrigger

--- Assets/Celeste-Movement/Scripts/Controller2D/Modules/Traps/TrapTrigger.cs
+++ Assets/Celeste-Movement/Scripts/Controller2D/Modules/Traps/TrapTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Carles.Engine2D {
 
@@ -11,12 +12,40 @@ namespace Carles.Engine2D {
     public float delayReset = 2f;
     public bool active = false;
 
-    private TextMesh info;
+    private TMP_Text label;
 
-    // private CharController2D c;
+    private CharController2D c;
 
     void Start() {
-      // c = GetComponent<CharController2D>();
+      c = GetComponent<CharController2D>();
+      label = GetComponentInChildren<TMP_Text>();
     }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+      // if (collision.tag != "Player") return;
+
+      // render click text and sound
+      // start wait coroutine
+      // tell trap to activate
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+      // if (collision.tag != "Player") return;
+
+    }
+
+    public void SetLabel(string str) {
+      label.text = str;
+    }
+
+
+    // public virtual IEnumerator SetLabel(string str) {
+    //   if (!label) { yield break; }
+
+    //   label.gameObject.SetActive(str != null);
+    //   if (str == null) { yield break; }
+    //   label.text = str;
+    // }
   }
 }
